20cb2c009af8e0d7bac4530b41634f03b2ea04d4
Ilgar Gamidov <151990428+braam76@users.noreply.github.com>
2025-01-12 09:59:08 +0300

    Added support for ido-mode and dired
    Added support for ido-mode (file-navigation in minibuffer) and dired. Also formatted a bit and commented purposes of faces

--- noirblaze.el
+++ noirblaze.el
@@ -20,12 +20,21 @@
 
   (custom-theme-set-faces
    'noirblaze
+   ;; Default colors, if other is not applied (I think)
    `(default ((t (:foreground ,gray-400 :background ,gray-900))))
-   `(cursor ((t (:background ,white))))
+   ;; Fringe is same color as background for default ^ value
    `(fringe ((t (:background ,gray-900))))
+   
+   ;; Cursor and minibuffer
+   `(cursor ((t (:background ,white))))
    `(mode-line ((t (:foreground ,gray-400 :background ,gray-800))))
+   `(minibuffer-prompt ((t (:foreground ,hot-pink :bold t))))
+
+   ;; Selecting region
    `(region ((t (:foreground ,gray-900 :background ,gray-600))))
    `(secondary-selection ((t (:background ,gray-700))))
+
+   ;; Basic keywords colors
    `(font-lock-builtin-face ((t (:foreground ,zinc-gray-600))))
    `(font-lock-comment-face ((t (:foreground ,gray-700))))
    `(font-lock-function-name-face ((t (:foreground ,white))))
@@ -34,8 +43,17 @@
    `(font-lock-type-face ((t (:foreground ,zinc-gray-500))))
    `(font-lock-constant-face ((t (:foreground ,hot-pink))))
    `(font-lock-variable-name-face ((t (:foreground ,white))))
-   `(minibuffer-prompt ((t (:foreground ,hot-pink :bold t))))
-   `(font-lock-warning-face ((t (:foreground "red" :bold t))))))
+   `(font-lock-warning-face ((t (:foreground "red" :bold t))))
+
+   ;; Ido mode (file navigation in minibuffer)
+   `(ido-subdir ((t (:foreground ,hot-pink))))
+   `(ido-first-match ((t (:foreground ,gray-400 :bold t))))
+   `(ido-only-match ((t (:foreground ,gray-400 :bold t))))
+
+   ;; Dired buffer
+   `(dired-directory ((t (:foreground ,hot-pink :background ,gray-900))))
+   `(dired-header ((t (:foreground ,hot-pink))))
+   ))
 
 ;;;###autoload
 (when (and load-file-name (boundp 'custom-theme-load-path))
